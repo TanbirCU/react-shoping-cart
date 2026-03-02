76d861af0317d67419adc8429ec34024bf74c9c1
TanbirCU <103535435+TanbirCU@users.noreply.github.com>
2026-03-03 00:17:22 +0600

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,12 @@ function Navbar(){
 function App() {
   
   return (
-    <>
-     
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
+    
+    </BrowserRouter>
   )
 }
 
