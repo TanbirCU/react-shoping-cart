ec3c005135a023b057e226568998e5a914eb42ac
TanbirCU <103535435+TanbirCU@users.noreply.github.com>
2026-03-07 23:40:29 +0600

    new changes

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -5,6 +5,15 @@ const CartContext=createContext()
 
 export const CartProvider=({children})=>{
     const [cart,setCart]=useState([])
+    const addTocart=(product)=>{
+        selectCart((prev)=>{
+            const existing=prev.find((item)=>item.id===product.id)
+            if(existing){
+
+            }
+            return [...prev,{...product}]
+        })
+    }
 
     return (
         <CartContext.Provider value={{ cart,setCart }}>
